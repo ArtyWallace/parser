84146711c7e4283060f76e76b7a5516476feb221
artem <artem.prudnikov57@gmail.com>
2021-04-28 16:02:27 +0300

    refactor

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 let csvToJson = require('convert-csv-to-json');
-const fs = require('fs')
 
 let fileInputName = __dirname + '/2021-04-01.csv';
-let fileOutputName = 'myOutputFile.json';
 let json;
 let result = [];
 
@@ -10,14 +8,19 @@ function parseCsv () {
   json = csvToJson.fieldDelimiter('|').getJsonFromCsv(fileInputName);
 }
 
-parseCsv();
-json.map(item => {
-  result.push({
-    point: item["Tablereport_source_Cell3"],
-    INN: item["Tablereport_source_Cell2"],
-    date: '2021-04-01',
-    earn: item["Tablereport_source_Cell4"],
-    spend: item["Tablereport_source_Cell5"]
+function generateResult () {
+  json.map(item => {
+    result.push({
+      point: item["Tablereport_source_Cell3"],
+      INN: item["Tablereport_source_Cell2"],
+      date: '2021-04-01',
+      earn: item["Tablereport_source_Cell4"],
+      spend: item["Tablereport_source_Cell5"]
+    })
   })
-})
+}
+
+parseCsv();
+generateResult()
+
 console.log(result);
